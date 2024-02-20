f41885ee392106e5f823b756418dd13f007c1f77
Carlos <carlosemaya@gmail.com>
2024-02-20 11:48:48 -0300

    getters e setters para todos os objetos

--- src/models.py
+++ src/models.py
@@ -1,88 +1,298 @@
-class Jogador:
-    def __init__(self, id, nick, total_kills, total_assists, total_deaths, total_stars, med_hs, total_score, total_matches, winrate):
+class JogadorTotalStats:
+    def __init__(self, id, nick, total_kills, total_assists, total_deaths, total_stars, total_hs, total_score, total_matches, winrate):
         self._id = id
         self._nick = nick
         self._total_kills = total_kills
         self._total_assists = total_assists
         self._total_deaths = total_deaths
         self._total_stars = total_stars
-        self._med_hs = med_hs
+        self._total_hs = total_hs
         self._total_score = total_score
         self._total_matches = total_matches
         self._winrate = winrate
 
-    #getter id
-    def get_id_jogador(self):
+    # Getters
+    @property
+    def id(self):
         return self._id
 
-    # Getter para nick
-    def get_nick(self):
+    @property
+    def nick(self):
         return self._nick
 
-    # Setter para nick
-    def set_nick(self, novo_nick):
-        self._nick = novo_nick
-
-    # Getter para total_kills
-    def get_total_kills(self):
+    @property
+    def total_kills(self):
         return self._total_kills
 
-    # Setter para total_kills
-    def set_total_kills(self, novo_total_kills):
-        self._total_kills = novo_total_kills
-
-    # Getter para total_assists
-    def get_total_assists(self):
+    @property
+    def total_assists(self):
         return self._total_assists
 
-    # Setter para total_assists
-    def set_total_assists(self, novo_total_assists):
-        self._total_assists = novo_total_assists
-
-    # Getter para total_deaths
-    def get_total_deaths(self):
+    @property
+    def total_deaths(self):
         return self._total_deaths
 
-    # Setter para total_deaths
-    def set_total_deaths(self, novo_total_deaths):
-        self._total_deaths = novo_total_deaths
-
-    # Getter para total_stars
-    def get_total_stars(self):
+    @property
+    def total_stars(self):
         return self._total_stars
 
-    # Setter para total_stars
-    def set_total_stars(self, novo_total_stars):
-        self._total_stars = novo_total_stars
+    @property
+    def total_hs(self):
+        return self._total_hs
 
-    # Getter para med_hs
-    def get_med_hs(self):
-        return self._med_hs
-
-    # Setter para med_hs
-    def set_med_hs(self, novo_med_hs):
-        self._med_hs = novo_med_hs
-
-    # Getter para total_score
-    def get_total_score(self):
+    @property
+    def total_score(self):
         return self._total_score
 
-    # Setter para total_score
-    def set_total_score(self, novo_total_score):
-        self._total_score = novo_total_score
-
-    # Getter para total_matches
-    def get_total_matches(self):
+    @property
+    def total_matches(self):
         return self._total_matches
 
-    # Setter para total_matches
-    def set_total_matches(self, novo_total_matches):
-        self._total_matches = novo_total_matches
-
-    # Getter para winrate
-    def get_winrate(self):
+    @property
+    def winrate(self):
         return self._winrate
 
-    # Setter para winrate
-    def set_winrate(self, novo_winrate):
-        self._winrate = novo_winrate
+    # Setters
+    @id.setter
+    def id(self, value):
+        self._id = value
+
+    @nick.setter
+    def nick(self, value):
+        self._nick = value
+
+    @total_kills.setter
+    def total_kills(self, value):
+        self._total_kills = value
+
+    @total_assists.setter
+    def total_assists(self, value):
+        self._total_assists = value
+
+    @total_deaths.setter
+    def total_deaths(self, value):
+        self._total_deaths = value
+
+    @total_stars.setter
+    def total_stars(self, value):
+        self._total_stars = value
+
+    @total_hs.setter
+    def total_hs(self, value):
+        self._total_hs = value
+
+    @total_score.setter
+    def total_score(self, value):
+        self._total_score = value
+
+    @total_matches.setter
+    def total_matches(self, value):
+        self._total_matches = value
+
+    @winrate.setter
+    def winrate(self, value):
+        self._winrate = value
+
+class JogadorStats:
+    def __init__(self, id, kills, assists, deaths, stars, hs, score, kda, time, jogador_total_stats_id, partida_id, partida_md3_id):
+        self._id = id
+        self._kills = kills
+        self._assists = assists
+        self._deaths = deaths
+        self._stars = stars
+        self._hs = hs
+        self._score = score
+        self._kda = kda
+        self._time = time
+        self._jogador_total_stats_id = jogador_total_stats_id
+        self._partida_id = partida_id
+        self._partida_md3_id = partida_md3_id
+
+    # Getters
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def kills(self):
+        return self._kills
+
+    @property
+    def assists(self):
+        return self._assists
+
+    @property
+    def deaths(self):
+        return self._deaths
+
+    @property
+    def stars(self):
+        return self._stars
+
+    @property
+    def hs(self):
+        return self._hs
+
+    @property
+    def score(self):
+        return self._score
+
+    @property
+    def kda(self):
+        return self._kda
+
+    @property
+    def time(self):
+        return self._time
+
+    @property
+    def jogador_total_stats_id(self):
+        return self._jogador_total_stats_id
+
+    @property
+    def partida_id(self):
+        return self._partida_id
+
+    @property
+    def partida_md3_id(self):
+        return self._partida_md3_id
+
+    # Setters
+    @id.setter
+    def id(self, value):
+        self._id = value
+
+    @kills.setter
+    def kills(self, value):
+        self._kills = value
+
+    @assists.setter
+    def assists(self, value):
+        self._assists = value
+
+    @deaths.setter
+    def deaths(self, value):
+        self._deaths = value
+
+    @stars.setter
+    def stars(self, value):
+        self._stars = value
+
+    @hs.setter
+    def hs(self, value):
+        self._hs = value
+
+    @score.setter
+    def score(self, value):
+        self._score = value
+
+    @kda.setter
+    def kda(self, value):
+        self._kda = value
+
+    @time.setter
+    def time(self, value):
+        self._time = value
+
+    @jogador_total_stats_id.setter
+    def jogador_total_stats_id(self, value):
+        self._jogador_total_stats_id = value
+
+    @partida_id.setter
+    def partida_id(self, value):
+        self._partida_id = value
+
+    @partida_md3_id.setter
+    def partida_md3_id(self, value):
+        self._partida_md3_id = value
+
+class Partida:
+    def __init__(self, id, jogo, mapa, rounds, vencedor_partida, md3_id):
+        self._id = id
+        self._jogo = jogo
+        self._mapa = mapa
+        self._rounds = rounds
+        self._vencedor_partida = vencedor_partida
+        self._md3_id = md3_id
+
+    # Getters
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def jogo(self):
+        return self._jogo
+
+    @property
+    def mapa(self):
+        return self._mapa
+
+    @property
+    def rounds(self):
+        return self._rounds
+
+    @property
+    def vencedor_partida(self):
+        return self._vencedor_partida
+
+    @property
+    def md3_id(self):
+        return self._md3_id
+
+    # Setters
+    @id.setter
+    def id(self, value):
+        self._id = value
+
+    @jogo.setter
+    def jogo(self, value):
+        self._jogo = value
+
+    @mapa.setter
+    def mapa(self, value):
+        self._mapa = value
+
+    @rounds.setter
+    def rounds(self, value):
+        self._rounds = value
+
+    @vencedor_partida.setter
+    def vencedor_partida(self, value):
+        self._vencedor_partida = value
+
+    @md3_id.setter
+    def md3_id(self, value):
+        self._md3_id = value
+
+class MD3:
+    def __init__(self, id, data, vencedor):
+        self._id = id
+        self._data = data
+        self._vencedor = vencedor
+
+    # Getters
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def vencedor(self):
+        return self._vencedor
+
+    # Setters
+    @id.setter
+    def id(self, value):
+        self._id = value
+
+    @data.setter
+    def data(self, value):
+        self._data = value
+
+    @vencedor.setter
+    def vencedor(self, value):
+        self._vencedor = value
